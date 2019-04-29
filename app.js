a785e413e517664d6fb2da85d7884a9b166b8c51
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-04-30 00:07:11 +0100

    Handled UX for adding a book and validating form

--- app.js
+++ app.js
@@ -21,7 +21,51 @@ UI.prototype.addBookToTable = function(book) {
 
   // Create tr element
   const row = document.createElement('tr');
-  console.log(row);
+
+  // Insert columns to the row
+  // This is done by adding the values of the book object in between
+  // td tags, which stand for data-cell... basically holding the cell details
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href='#' class='delete'>x</a></td>    
+  `;
+  list.appendChild(row);
+};
+
+// Show Alerts
+UI.prototype.showAlert = function(message, className) {
+  // Create the element
+  const div = document.createElement('div');
+
+  // Add class
+  div.className = `alert ${className}`;
+
+  // Add the message to the element created
+  div.appendChild(document.createTextNode(message));
+
+  // Insert into the DOM
+  // Get a parent
+  const container = document.querySelector('.container');
+
+  // Get another parent - This would be the element we would be insert our alert before
+  const form = document.querySelector('#book-form');
+
+  // Inserting alert into the DOM now
+  container.insertBefore(div, form);
+
+  // Timeout after 3secs
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000);
+};
+
+// The method to clear form fields after submission
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
 };
 
 // Event Listeners
@@ -42,7 +86,10 @@ function submittedForm(e) {
   // Instantiate a UI object
   const ui = new UI();
 
-  ui.addBookToTable(book);
+  // Validate form entries/submission
+  title === '' || author === '' || isbn === ''
+    ? ui.showAlert('Please fill in all fields', 'error')
+    : (ui.addBookToTable(book), ui.showAlert('Book added successfully', 'success'), ui.clearFields());
 
   e.preventDefault();
 }
